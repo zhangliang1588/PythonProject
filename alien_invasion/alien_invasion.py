c5f812dc2303870ab8e58276471220f5a59954f6
Zhang <13052332986@163.com>
2018-05-03 23:09:18 +0800

    update aliens

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -8,6 +8,7 @@ Created on Tue Apr 24 21:33:20 2018
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -19,16 +20,20 @@ def run_game():
             (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
     
-    #创建一艘飞船
+    #创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings,screen)
-    #创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
+    
+    #创建外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+    
     
     #开始游戏的主循环
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullet(bullets)        
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
         
 run_game()
